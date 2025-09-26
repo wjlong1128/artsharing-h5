e03431dcf73b4995d10a7eb027c58815c4ce6842
wjlong <13939368045@qq.com>
2025-09-26 22:44:36 +0800

    first commit

---
+++ lib/db/schema.ts
@@ -0,0 +1,8 @@
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+// npx drizzle-kit push
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  age: integer().notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+});
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,37 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // 图片服务器
+  images: {
+    remotePatterns: [
+      {
+        hostname: "*.hdslb.com",
+      },
+    ],
+  },
+   async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/h5/index.html',
+        permanent: false, // 临时重定向
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      
+      {
+        source: '/h5-api/:path*',  // 匹配 /h5-api/ 开头的所有路径
+        destination: 'https://artsharing.vhost.chengzhanheng.cn/:path*',  // 转发时去掉 /h5-api
+      },
+      // 如果需要处理根路径
+      {
+        source: '/h5-api',  // 匹配 /h5-api（没有后续路径）
+        destination: 'https://目标网站.com/',  // 转发到根路径
+      },
+    ];
+  },
 };
 
 export default nextConfig;
